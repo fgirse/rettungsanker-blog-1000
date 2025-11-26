76094da38ebe797127c7b5af5f99e6c820b0bb71
fgirse <fgirs@bluewin.ch>
2025-11-26 09:34:51 +0100

    modify create-post

--- app/dashboard/create-post/page.jsx
+++ app/dashboard/create-post/page.jsx
@@ -132,9 +132,9 @@ export default function CreatePostPage() {
               }
             >
               <option value='uncategorized'>Select a category</option>
-              <option value='javascript'>JavaScript</option>
-              <option value='reactjs'>React.js</option>
-              <option value='nextjs'>Next.js</option>
+              <option value='rettungsanker-freiburg'>Rettungsanker-Freiburg</option>
+              <option value='bundesliga'>Bundesliga</option>
+              <option value='sc-freiburg'>SC Freiburg</option>
             </Select>
           </div>
           <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
