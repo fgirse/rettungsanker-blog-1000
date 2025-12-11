aebdb1429b69928af0e78741b61ee480578b9c9b
fgirse <fgirs@bluewin.ch>
2025-12-11 06:30:52 +0100

    fix sign-in sign-out

--- app/sign-up/[[...sign-up]]/page.jsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from '@clerk/nextjs';
-
-export default function SignUpPage() {
-  return (
-    <div className='flex items-center justify-center p-3 min-h-screen'>
-      <SignUp />
-    </div>
-  );
-}
