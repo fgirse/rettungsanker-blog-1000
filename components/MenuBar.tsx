65bd23af45fdb9f561dd379241b33136cd588797
fgirse <fgirs@bluewin.ch>
2025-12-11 18:14:01 +0100

    fix new attemp navigation

--- components/MenuBar.tsx
+++ components/MenuBar.tsx
@@ -1,130 +1,52 @@
-
 /**
  * Renders a user button component provided by the Clerk.js library.
  * This component allows users to interact with their account, such as
  * logging out or accessing their profile.
  */
-
-'use client';
-
-import { UserButton, useAuth } from "@clerk/nextjs";
-import React from "react";
-import Image from "next/image";
-import Bulleye from "../public/Assets/Svg/Bulleye.svg";
-import LogoNeu from "../public/Assets/Img/LogoNeu.png";
-import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
-import BottomNavigation from "./BottomNavigation";
+import React from "react";
 
-export default function MenuBar() {
-  const { userId } = useAuth();
-
+const Navbar = async () => {
+  const { userId } = await auth();
   return (
-    <>
-    <Navbar fluid rounded className="py-[4vh] bg-cyan-950 rounded-b-xl lg:bg-[url('/Assets/Svg/Wood3.svg')] lg:bg-cover lg:bg-center lg:bg-no-repeat">
-      <NavbarBrand as={Link} href="/" className="relative left-[2vw]">
-        <Image src={LogoNeu} width={120} height={120} className="mr-3 h-6 sm:h-9" alt="RettungsankerLogo" />
-      </NavbarBrand>
-
-      <div className="flex items-center gap-3 lg:hidden ml-auto">
-        <NavbarToggle className="text-white hover:bg-orange-400" />
-      </div>
-
-      {/* Desktop Navigation - Always visible */}
-      <div className="hidden lg:flex items-center gap-3">
-        <div className="flex items-center gap-2 lg:gap-5">
-          <Image src={Bulleye} className="w-5 h-5 lg:w-6 lg:h-6 xl:w-7 xl:h-7" alt="Bulleye" width={40} height={40} />
-          <Link className="uppercase hover:bg-orange-400 lg:text-xl xl:text-2xl font-bowlby text-white no-underline" href="/">Home</Link>
+    <div className="bg-cyan-950 rounded-b-xl">
+      <ul className="flex justify-between py-4 px-6">
+        <div>
+          <Link href="/">
+            <li>Home</li>
+          </Link>
         </div>
-        <div className="flex items-center gap-2 lg:gap-5">
-          <Image src={Bulleye} className="w-5 h-5 lg:w-6 lg:h-6 xl:w-7 xl:h-7" alt="Bulleye" width={40} height={40} />
-          <Link className="uppercase hover:bg-orange-400 lg:text-xl xl:text-2xl font-bowlby text-white no-underline" href="/about">über uns</Link>
+        <div className="flex items-center">
+          <Link href="/client">
+            <li>Client Page</li>
+          </Link>
         </div>
-        <div className="flex items-center gap-2 lg:gap-5">
-          <Image src={Bulleye} className="w-5 h-5 lg:w-6 lg:h-6 xl:w-7 xl:h-7" alt="Bulleye" width={40} height={40} />
-          <Link className="uppercase hover:bg-orange-400 lg:text-xl xl:text-2xl font-bowlby text-white no-underline" href="/drinks">angebot</Link>
+        <div className="flex gap-6 items-center">
+          {!userId ? (
+            <>
+              <Link href="/sign-in">
+                <li>Login</li>
+              </Link>
+              <Link href="/sign-up">
+                <li>Sign Up</li>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link href="/profile">
+                <li>Profile</li>
+              </Link>
+              <li className="flex items-center">
+                <UserButton />
+              </li>
+            </>
+          )}
         </div>
-        <div className="flex items-center gap-2 lg:gap-5">
-          <Image src={Bulleye} className="w-5 h-5 lg:w-6 lg:h-6 xl:w-7 xl:h-7" alt="Bulleye" width={40} height={40} />
-          <Link className="uppercase hover:bg-orange-400 lg:text-xl xl:text-2xl font-bowlby text-white no-underline" href="/sportarena">sportarena</Link>
-        </div>
-        <div className="flex items-center gap-2 lg:gap-5">
-          <Image src={Bulleye} className="w-5 h-5 lg:w-6 lg:h-6 xl:w-7 xl:h-7" alt="Bulleye" width={40} height={40} />
-          <Link className="uppercase hover:bg-orange-400 lg:text-xl xl:text-2xl font-bowlby text-white no-underline" href="/wohin">wohin?</Link>
-        </div>
-        <div className="flex items-center gap-2 lg:gap-5">
-          <Image src={Bulleye} className="w-5 h-5 lg:w-6 lg:h-6 xl:w-7 xl:h-7" alt="Bulleye" width={40} height={40} />
-          <Link className="uppercase hover:bg-orange-400 lg:text-xl xl:text-2xl font-bowlby text-white no-underline" href="/client">blog</Link>
-        </div>
-
-        {/* Auth buttons for desktop */}
-        {!userId ? (
-          <>
-            <Link className="text-white bg-slate-400 border px-2 py-1 rounded-lg hover:bg-orange-500 no-underline" href="/sign-in">
-              Anmelden
-            </Link>
-            <Link className="text-white bg-slate-600 border px-2 py-1 rounded-lg hover:bg-orange-500 no-underline" href="/sign-up">
-              Registrieren
-            </Link>
-          </>
-        ) : (
-          <>
-            <Link className="text-white no-underline" href="/profile">
-              Profile
-            </Link>
-            <UserButton />
-          </>
-        )}
-      </div>
-
-      {/* Mobile Navigation - Collapsible */}
-      <NavbarCollapse className="lg:hidden">
-        <div className="flex items-center gap-2 lg:gap-5">
-          <Image src={Bulleye} className="w-5 h-5 lg:w-6 lg:h-6 xl:w-7 xl:h-7" alt="Bulleye" width={40} height={40} />
-          <NavbarLink as={Link} className="uppercase hover:bg-orange-400 lg:text-xl xl:text-2xl font-bowlby text-white" href="/">Home</NavbarLink>
-        </div>
-        <div className="flex items-center gap-2 lg:gap-5">
-          <Image src={Bulleye} className="w-5 h-5 lg:w-6 lg:h-6 xl:w-7 xl:h-7" alt="Bulleye" width={40} height={40} />
-          <NavbarLink as={Link} className="uppercase hover:bg-orange-400 lg:text-xl xl:text-2xl font-bowlby text-white" href="/about">über uns</NavbarLink>
-        </div>
-        <div className="flex items-center gap-2 lg:gap-5">
-          <Image src={Bulleye} className="w-5 h-5 lg:w-6 lg:h-6 xl:w-7 xl:h-7" alt="Bulleye" width={40} height={40} />
-          <NavbarLink as={Link} className="uppercase hover:bg-orange-400 lg:text-xl xl:text-2xl font-bowlby text-white" href="/drinks">angebot</NavbarLink>
-        </div>
-        <div className="flex items-center gap-2 lg:gap-5">
-          <Image src={Bulleye} className="w-5 h-5 lg:w-6 lg:h-6 xl:w-7 xl:h-7" alt="Bulleye" width={40} height={40} />
-          <NavbarLink as={Link} className="uppercase hover:bg-orange-400 lg:text-xl xl:text-2xl font-bowlby text-white" href="/sportarena">sportarena</NavbarLink>
-        </div>
-        <div className="flex items-center gap-2 lg:gap-5">
-          <Image src={Bulleye} className="w-5 h-5 lg:w-6 lg:h-6 xl:w-7 xl:h-7" alt="Bulleye" width={40} height={40} />
-          <NavbarLink as={Link} className="uppercase hover:bg-orange-400 lg:text-xl xl:text-2xl font-bowlby text-white" href="/wohin">wohin?</NavbarLink>
-        </div>
-        <div className="flex items-center gap-2 lg:gap-5">
-          <Image src={Bulleye} className="w-5 h-5 lg:w-6 lg:h-6 xl:w-7 xl:h-7" alt="Bulleye" width={40} height={40} />
-          <NavbarLink as={Link} className="uppercase hover:bg-orange-400 lg:text-xl xl:text-2xl font-bowlby text-white" href="/client">blog</NavbarLink>
-        </div>
-
-        {/* Auth buttons for mobile */}
-        {!userId ? (
-          <>
-            <Link className="text-white bg-slate-400 border px-2 py-1 rounded-lg hover:bg-orange-500 no-underline text-center" href="/sign-in">
-              Anmelden
-            </Link>
-            <Link className="text-white bg-slate-600 border px-2 py-1 rounded-lg hover:bg-orange-500 no-underline text-center" href="/sign-up">
-              Registrieren
-            </Link>
-          </>
-        ) : (
-          <>
-            <Link className="text-white no-underline" href="/profile">
-              Profile
-            </Link>
-            <UserButton />
-          </>
-        )}
-      </NavbarCollapse>
-    </Navbar>
-    <BottomNavigation userId={null} />
-  </>
+      </ul>
+    </div>
   );
-}
+};
+
+export default Navbar;
