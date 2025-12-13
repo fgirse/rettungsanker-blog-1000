21269bf714c60e3ec1c81d763175560cfd3ffb8f
fgirse <fgirs@bluewin.ch>
2025-12-13 11:00:54 +0100

    layout/impressum

--- app/impressum/page.tsx
+++ app/impressum/page.tsx
@@ -3,10 +3,31 @@ import React from 'react'
 const Impressum = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-700 p-8">
-      <h1 className="text-4xl text-blue-600 lg:text-6xl text-shadow-lg uppercase headingA font-bold mb-4">Impressum</h1>
-      <p className="text-xl text-gray-700">
-        Impressum: TV Live Bundesliga
-      </p>
+      <h1>Impressum</h1>
+
+<p>Rettungsanker-Freiburg<br />
+Adelhauser-Strasse  7c<br />
+79098 Freiburg</p>
+
+<p><strong>Vertreten durch:</strong><br />
+Michael Schreck</p>
+
+<h2>Kontakt</h2>
+<p>Telefon: +49 761 383 867 47<br />
+E-Mail: rettungsanker-freiburg@gmx.de</p>
+
+<h2>Umsatzsteuer-ID</h2>
+<p>Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a Umsatzsteuergesetz:<br />
+DE 142 114900</p>
+
+<h2>Redaktionell verantwortlich</h2>
+<p>Rettungsanker Freiburg<br />
+Michael Schreck, Staufener-Strasse 78, 79189 Bad Krozingen</p>
+
+<h2>Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle</h2>
+<p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+
+<p>Quelle: <a href="https://www.e-recht24.de">e-recht24.de</a></p>
     </div>
   )
 }
