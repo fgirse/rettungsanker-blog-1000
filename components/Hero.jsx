1e11d75ee741363e65391c8ad7c40aca89750314
fgirse <fgirs@bluewin.ch>
2025-12-13 07:35:50 +0100

    layout-hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AnimatedHero from '@/components/animated-logo';
 const Hero = () => {
   return (
-    <section className='mt-[-3vh] bg-[url("/Assets/Img/lighthouse.png")] bg-cover bg-no-repeat bg-center lg:mt-0 lg:bg-[url("/Assets/Svg/5555.svg")] lg:bg-contain lg:bg-no-repeat lg:bg-center h-screen w-full flex items-center justify-center'>
+    <section className='mt-[-8vh] bg-[url("/Assets/Img/lighthouse.png")] bg-cover bg-no-repeat bg-center lg:mt-0 lg:bg-[url("/Assets/Svg/5555.svg")] lg:bg-contain lg:bg-no-repeat lg:bg-center h-screen w-full flex items-center justify-center'>
       <AnimatedHero />
     </section>
   )
